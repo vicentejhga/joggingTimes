59d91bebfb6ba8cbb433e2232c1570a65d1e3230
Vicente Jose Hernandez Garcia <vicentejhga@gmail.com>
2017-11-28 07:49:11 +0000

    Added more paths in global config/paths.js

--- config/paths.js
+++ config/paths.js
@@ -1,3 +1,7 @@
-
+global.MODEL_PATH = ROOT_PATH + 'model/';
+global.CONTROLLER_PATH = ROOT_PATH + 'controller/';
+global.HANDLER_PATH = ROOT_PATH + 'handler/';
+global.BASE_PACKAGE_PATH = ROOT_PATH + 'base/';
+global.MANAGER_PATH = ROOT_PATH + 'manager/';
 global.ROUTE_PATH = ROOT_PATH + 'route/';
-
+global.CONFIG_BASE_PATH = __dirname + '/';
