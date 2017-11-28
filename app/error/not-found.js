2164d674853921cd83580feba5ad21d37cb44934
Vicente Jose Hernandez Garcia <vicentejhga@gmail.com>
2017-11-28 10:26:40 +0000

    Added error folder

---
+++ app/error/not-found.js
@@ -0,0 +1,11 @@
+
+'use strict';
+const BaseError = require(APP_ERROR_PATH + 'base');
+
+class NotFoundError extends BaseError {
+    constructor(message) {
+        super(message, 404);
+    }
+}
+
+module.exports = NotFoundError;
---
+++ app/error/not-implemented.js
@@ -0,0 +1,12 @@
+
+
+'use strict';
+const BaseError = require(APP_ERROR_PATH + 'base');
+
+class NotImplemented extends BaseError {
+    constructor(message) {
+        super(message, 501);
+    }
+}
+
+module.exports = NotImplemented;
