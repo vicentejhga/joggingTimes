4e06a6e4c7185cc1b30e92bd750f17a9718a42cf
Vicente Jose Hernandez Garcia <vicentejhga@gmail.com>
2017-11-29 08:00:55 +0000

    delete old files out of back folder

--- app/base/base-autobind.js
+++
@@ -1,9 +0,0 @@
-
-const autoBind = require('auto-bind');
-
-class BaseAutoBindedClass {
-    constructor() {
-        autoBind(this);
-    }
-}
-module.exports = BaseAutoBindedClass;
--- config/jwt-options.js
+++
@@ -1,6 +0,0 @@
-
-module.exports = {
-    issuer: "",
-    audience: "",
-    algorithm: "RS256"
-};
