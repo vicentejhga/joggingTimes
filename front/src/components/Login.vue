53cd1747285871b652da74fae091d8f14ac37774
Vicente <vicente@carmendata.co.uk>
2017-12-04 13:21:15 +0000

    Using webpack for front end tool

---
+++ front/src/components/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  	<div class="loginForm">
+      	<h1>Login</h1>
+      	<input type = "text" placeholder="E-mail" />
+      	<input type = "password" placeholder="Password" />
+  		<button v-on:click="signIn">Sign in</button>
+  	<div><p>New user? <a href="#/test">create new account</a></p></div>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+  		signIn:function() {
+  			alert("ok");
+  		}
+  }
+}
+</script>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ front/src/components/Register.vue
@@ -0,0 +1,36 @@
+<template>
+  	<div class="loginForm">
+      	<h1>New user</h1>
+      	<input type = "text" placeholder="First name" />
+        <input type = "text" placeholder="Second name" />
+        <input type = "text" placeholder="E-mail" />
+      	<input type = "password" placeholder="Password" />
+  		  <button v-on:click="signIn">Create account</button>
+  	
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  name: 'Register',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+  		signIn:function() {
+  			alert("ok");
+  		}
+  }
+}
+</script>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
